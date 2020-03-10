fcf865a2e5efcfd043b3453b32f3a7809eefa53d
unknown <sbd12b@gmail.com>
2020-03-10 12:10:05 -0400

    with updated class info

--- src/hello.js
+++ src/hello.js
@@ -1,14 +1,26 @@
-ReactDOM.render(
+// ReactDOM.render(
 
-    React.createElement(
-        'h1',
-        {
-            className: 'class-1',
-            style: {
-                color: 'red'
-            }
-        },
-        "Hello World"
-    ),
-    document.getElementById('app')
+//     React.createElement(
+//         'h1',
+//         {
+//             className: 'class-1',
+//             style: {
+//                 color: 'red'
+//             }
+//         },
+//         "Hello World"
+//     ),
+//     document.getElementById('app')
+// )
+class App extends React.Component {
+    render() {
+        return(
+            <h1>Hello World</h1>
+        )
+    }
+}
+ReactDOM.render(
+    React.createElement(App, {
+        name: "test"
+    }), document.getElementById('app')
 )
